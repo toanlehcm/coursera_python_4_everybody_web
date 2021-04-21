06cc6564788858c38a1209e4831981f557d0264c
ToanLeThanh <thanhtoanhcm1602@gmail.com>
2021-04-21 18:58:44 +0700

    - update create behavior.

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from flask import Flask, render_template, request
 import sqlite3 as sql
 app = Flask(__name__)
 
-# @app.route('/')
-# def home():
-#     return render_template('home.html')
-    
+
 @app.route('/')
 def list():
     con = sql.connect("database.db")
@@ -13,30 +10,68 @@ def list():
 
     cur = con.cursor()
     cur.execute("SELECT * FROM students")
-    cur.execute("SELECT \"_rowid_\",* FROM \"main\".\"students\"  ORDER BY \"id\" ASC LIMIT 0, 49999;")
+    cur.execute(
+        "SELECT \"_rowid_\",* FROM \"main\".\"students\"  ORDER BY \"id\" ASC LIMIT 0, 49999;")
 
     rows = cur.fetchall()
-    return render_template("list.html",rows = rows)
+    return render_template("list.html", rows=rows)
+
 
 @app.route('/create')
 def create():
     return render_template('create.html')
-    
-@app.route('/edit',methods = ['POST', 'GET'])
+
+
+@app.route('/store', methods=['POST', 'GET'])
+def store():
+    if request.method == 'POST':
+        try:
+            id = request.form['id']
+            name = request.form['name']
+            email = request.form['email']
+            hometown = request.form['hometown']
+            dob = request.form['dob']
+            score = request.form['score']
+            dict_info = {"name": name, "email": email,
+                         "hometown": hometown, "dob": dob, "score": score}
+            with sql.connect("database.db") as con:
+                con.row_factory = sql.Row
+                cur = con.cursor()
+                cur.execute("INSERT INTO students (id, name, email, hometown, dob, score) VALUES (?,?,?,?,?,?)",
+                            (id, name, email, hometown, dob, score))
+                con.commit()
+        except:
+            con.rollback()
+
+        finally:
+            con = sql.connect("database.db")
+            con.row_factory = sql.Row
+
+            cur = con.cursor()
+            cur.execute("select * from students")
+            cur.execute(
+                "select \"_rowid_\",* from \"main\".\"students\" order by \"id\" asc limit 0, 49999;")
+
+            rows = cur.fetchall()
+            return render_template("list.html", rows=rows)
+
+
+@app.route('/edit', methods=['POST', 'GET'])
 def update_form():
     con = sql.connect("database.db")
     con.row_factory = sql.Row
 
     cur = con.cursor()
     cur.execute("SELECT * FROM students")
-    cur.execute("SELECT \"_rowid_\",* FROM \"main\".\"students\"  ORDER BY \"id\" ASC LIMIT 0, 49999;")
-    
+    cur.execute(
+        "SELECT \"_rowid_\",* FROM \"main\".\"students\"  ORDER BY \"id\" ASC LIMIT 0, 49999;")
+
     rows = cur.fetchall()
-    
-    return render_template("update_form.html",rows = rows)
-    
-    
-@app.route('/update',methods = ['POST', 'GET'])
+
+    return render_template("update_form.html", rows=rows)
+
+
+@app.route('/update', methods=['POST', 'GET'])
 def update():
     if request.method == 'POST':
         try:
@@ -46,19 +81,20 @@ def update():
             hometown = request.form['hometown']
             dob = request.form['dob']
             score = request.form['score']
-            dict_info = {"name":name,"email":email,"hometown":hometown,"dob":dob,"score":score}
+            dict_info = {"name": name, "email": email,
+                         "hometown": hometown, "dob": dob, "score": score}
             with sql.connect("database.db") as con:
                 con.row_factory = sql.Row
                 cur = con.cursor()
                 cur.execute("SELECT * FROM students")
-                
+
                 rows = cur.fetchall()
                 id_list = []
                 for row in rows:
                     id_list.append(row["id"])
                 if id not in id_list:
                     cur.execute("INSERT INTO students (id,name,email,hometown,dob,score) \
-                       VALUES (?,?,?,?,?,?)",(id,name,email,hometown,dob,score) )
+                       VALUES (?,?,?,?,?,?)", (id, name, email, hometown, dob, score))
                 else:
                     list_key = []
                     list_value = []
@@ -66,29 +102,32 @@ def update():
                         list_value.append(values)
                     for key in dict_info.keys():
                         list_key.append(key)
-                        
+
                     for i in list_value:
                         if i == "":
                             pass
                         else:
                             key = (list_key[list_value.index(i)])
-                            cur.execute("UPDATE students SET %s = '%s' WHERE id = %s;"%(key,i,id))
+                            cur.execute(
+                                "UPDATE students SET %s = '%s' WHERE id = %s;" % (key, i, id))
                 con.commit()
         except:
             con.rollback()
-            
+
         finally:
             con = sql.connect("database.db")
             con.row_factory = sql.Row
 
             cur = con.cursor()
             cur.execute("SELECT * FROM students")
-            cur.execute("SELECT \"_rowid_\",* FROM \"main\".\"students\"  ORDER BY \"id\" ASC LIMIT 0, 49999;")
+            cur.execute(
+                "SELECT \"_rowid_\",* FROM \"main\".\"students\"  ORDER BY \"id\" ASC LIMIT 0, 49999;")
 
             rows = cur.fetchall()
-            return render_template("list.html",rows = rows)
+            return render_template("list.html", rows=rows)
 
-@app.route('/delete_form',methods = ['POST', 'GET'])
+
+@app.route('/delete_form', methods=['POST', 'GET'])
 def delete_form():
 
     con = sql.connect("database.db")
@@ -96,12 +135,14 @@ def delete_form():
 
     cur = con.cursor()
     cur.execute("SELECT * FROM students")
-    cur.execute("SELECT \"_rowid_\",* FROM \"main\".\"students\"  ORDER BY \"id\" ASC LIMIT 0, 49999;")
+    cur.execute(
+        "SELECT \"_rowid_\",* FROM \"main\".\"students\"  ORDER BY \"id\" ASC LIMIT 0, 49999;")
 
     rows = cur.fetchall()
-    return render_template('delete_form.html',rows = rows)
-    
-@app.route('/delete',methods = ['POST', 'GET'])
+    return render_template('delete_form.html', rows=rows)
+
+
+@app.route('/delete', methods=['POST', 'GET'])
 def delete():
     if request.method == 'POST':
         try:
@@ -109,22 +150,24 @@ def delete():
 
             with sql.connect("database.db") as con:
                 cur = con.cursor()
-                cur.execute("DELETE FROM students WHERE id = %s"%id, )
+                cur.execute("DELETE FROM students WHERE id = %s" % id, )
 
             con.commit()
         except:
             con.rollback()
-            
+
         finally:
             con = sql.connect("database.db")
             con.row_factory = sql.Row
 
             cur = con.cursor()
             cur.execute("SELECT * FROM students")
-            cur.execute("SELECT \"_rowid_\",* FROM \"main\".\"students\"  ORDER BY \"id\" ASC LIMIT 0, 49999;")
+            cur.execute(
+                "SELECT \"_rowid_\",* FROM \"main\".\"students\"  ORDER BY \"id\" ASC LIMIT 0, 49999;")
 
             rows = cur.fetchall()
-            return render_template("list.html",rows = rows)
+            return render_template("list.html", rows=rows)
+
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True)
